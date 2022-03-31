a1cc47d30b9c7ed88c74535ada241862bd612643
ansAlgorithms <102295315+ansAlgorithms@users.noreply.github.com>
2022-03-31 11:45:34 +0530

    Update verifyToken.js

--- Controller/verifyToken.js
+++ Controller/verifyToken.js
@@ -2,12 +2,13 @@ const jwt = require('jsonwebtoken')
 module.exports = (req,res,next)=>{
     const token = req.header('auth-token');
     if(!token){
-        res.json({message : `Please Login to see the post`});
+        res.status(401).json({message : `Please Login to see the post`});
     }
-    else{
-        const verified = jwt.verify(token,process.env.TOKEN_SECRET);
+    try {
+        const verified = jwt.verify(token, process.env.TOKEN_SECRET);
         req.user = verified;
-        next()
-        if(verified===false) res.json({message : `Invalid token`})
+        next();
+    } catch {
+        res.status(401).json({ message: `Invalid token` });
     }
 }
